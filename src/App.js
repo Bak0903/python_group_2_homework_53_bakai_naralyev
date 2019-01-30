793a76347e83b5579a0620dafe848b20c30e69b7
bak0903 <bak0903@mail.ru>
2019-01-30 17:26:10 +0600

    Changed App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Lotto from "./Lotto/Lotto.js";
+
+function compareNumeric(a, b) {
+  return a - b;
+}
+
+function lottoCombination() {
+    let combo = [];
+    for (let i = 0; i < 5; i++) {
+        combo[i] = Math.floor(Math.random() * 28) + 5
+    }
+    console.log(combo);
+    combo.sort(compareNumeric);
+    console.log(combo);
+    return combo;
+}
+
 
 class App extends Component {
+
+    state = {numbers: lottoCombination()};
+
+    newCombo = () => {
+        console.log('Кнопка нажата');
+        let newState = {...this.state};
+        newState.numbers = lottoCombination();
+        this.setState(newState)
+    };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <button className={'btn'} onClick={() =>(this.newCombo())}>New numbers</button>
+          <Lotto combo = {this.state}/>
       </div>
     );
   }
